9283bc74f43f5c6ad270770537a21880d6be874e
thersgo <henrikangro@gmail.com>
2025-10-01 21:25:23 +0300

    Navigation standardisation and Decent miner game update
    global:
    - Updated and standardised headers,
    - Standardised indentation (2 spaces per indent, always).
    
    miner:
    - Did a bit of `<style>`ing and rephrasing in miner,
    - Made bots asynchronous ('cause it looks cooler),
    - Added ability to upgrade drills.

--- miner.js
+++ miner.js
@@ -1,35 +1,55 @@
-let mineButton = document.getElementById("mineButton");
-let fuelCount  = document.getElementById("fuelCount");
-let botCount   = document.getElementById("botCount");
-let buyBots    = document.getElementById("buyBots");
+let mineButton    = document.getElementById("mineButton");
+let fuelCount     = document.getElementById("fuelCount");
+let botCount      = document.getElementById("botCount");
+let buyBots       = document.getElementById("buyBots");
+let drillLevel    = document.getElementById("drillLevel");
+let upgradeDrills = document.getElementById("upgradeDrills");
 
 let fuel = 0;
+let mineSpeed = 1;
+let drillCost = 32;
 
 let bots = 0;
 let botCost = 32;
 
+let round = (n) => {return Math.round(n*10)/10};
+
 function updateTexts() {
-    fuelCount.innerText = `You have mined ${fuel} TJf (terajoules of fossil fuels),`;
-    botCount.innerText  = `You have ${bots} worker bot${bots!=1?"s":""} mining ${bots}TJf/sec.`
+  fuelCount.innerText  = `You have mined ${round(fuel)} TJf (terajoules of fossil fuels).`;
+  botCount.innerText   = `You have ${round(bots)} other worker bot${bots!=1?"s":""} mining ${round(bots*mineSpeed)}TJf/sec.`;
+  drillLevel.innerText = `Your drills currently mine ${round(mineSpeed)}TJf per hit`;
+  buyBots.innerText = `build a helper worker bot for ${round(botCost)}TJf`;
+  upgradeDrills.innerText = `Upgrade all drills for ${round(drillCost)}TJf`
 }
 
 function clickFuelmine() {
-    fuel++;
-    updateTexts();
+  fuel += mineSpeed;
+  updateTexts();
 }
 
 function buyBot() {
-    if (fuel >= botCost) {
-        fuel -= botCost;
-        bots++;
-        botCost = Math.round(botCost * 1.5); 
+  if (fuel >= botCost) {
+    fuel -= botCost;
+    bots++;
 
-        updateTexts();
-        buyBots.innerText = "build a worker bot using " + botCost + "TJf";
-    }
+    botCost *= 1.25;
+
+    setInterval(function() {
+      fuel += mineSpeed;
+      updateTexts();
+    }, 1000)
+
+    updateTexts();
+  }
 }
 
-setInterval(function() {
-    fuel += bots;
+function upgradeDrill() {
+  if (fuel >= drillCost) {
+    fuel -= drillCost;
+    mineSpeed *= 1.1;
+
+    drillCost *= 1.5;
+
     updateTexts();
-}, 1000)
+  }
+}
